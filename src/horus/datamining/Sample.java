44421ba8c574c35a4261949d77357142935b7804
expinc <expinc@outlook.com>
2018-08-20 21:11:01 +0800

    Refactory
    Change names of classes for sample and test

---
+++ src/horus/datamining/Sample.java
@@ -0,0 +1,43 @@
+package horus.datamining;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import horus.datamining.model.*;
+import horus.datamining.model.feature.FeatureVector;
+
+
+public final class Sample
+{
+	public static void main(String[] args) throws Exception
+	{
+		// Set environment: set path that contains .model and .arff files
+		Environment environment = new EnvironmentImpl();
+		environment.setModelPath("D:/my-git/data-mining/DataMining/models/");
+		
+		// Create model accordingly
+		Model model = new PurchasePricePrediction(environment);
+		
+		// Create feature vector
+		// Schema could be found in corresponding .arff files
+		// Typically, the last feature is the result feature
+		FeatureVector featureVector = model.createFeatureVector();
+		LocalDate date = LocalDate.of(2017, 4, 1);
+		featureVector.setValue("Year", date.getYear());
+		featureVector.setValue("Month", date.getMonthValue());
+		featureVector.setValue("Day", date.getDayOfMonth());
+		int dayOfWeek = date.getDayOfWeek().getValue() % DayOfWeek.SUNDAY.getValue();
+		featureVector.setValue("WeekDay", dayOfWeek);
+		System.out.println("Feature Vector" +
+				": Year - " + date.getYear() +
+				", Month - " + date.getMonthValue() +
+				", Day - " + date.getDayOfMonth() +
+				", WeekDay - " + dayOfWeek);
+		
+		
+		// Solve the feature vector by the model
+		// Suggestion contains several fields
+		// Typically the last feature of the feature schema
+		Suggestion suggestion = model.solve(featureVector);
+		System.out.println("Predicted price: " + suggestion.getFieldValue("Price"));
+	}
+}
--- src/horus/datamining/Test.java
+++ src/horus/datamining/Test.java
@@ -2,7 +2,9 @@ package horus.datamining;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
-import horus.datamining.model.*;
+import horus.datamining.model.Model;
+import horus.datamining.model.PurchasePricePrediction;
+import horus.datamining.model.Suggestion;
 import horus.datamining.model.feature.FeatureVector;
 
 
@@ -10,28 +12,31 @@ public final class Test
 {
 	public static void main(String[] args) throws Exception
 	{
-		// Set environment: set path that contains .model and .arff files
+		testPurchasePricePrediction();
+	}
+	
+	
+	private static void testPurchasePricePrediction() throws Exception
+	{
 		Environment environment = new EnvironmentImpl();
 		environment.setModelPath("D:/my-git/data-mining/DataMining/models/");
-		
-		// Create model accordingly
+
 		Model model = new PurchasePricePrediction(environment);
-		
-		// Create feature vector
-		// Schema could be found in corresponding .arff files
-		// Typically, the last feature is the result feature
-		FeatureVector featureVector = model.createFeatureVector();
+
 		LocalDate date = LocalDate.of(2017, 4, 1);
-		featureVector.setValue("Year", date.getYear());
-		featureVector.setValue("Month", date.getMonth());
-		featureVector.setValue("Day", date.getDayOfMonth());
-		int dayOfWeek = date.getDayOfWeek().getValue() % DayOfWeek.SUNDAY.getValue();
-		featureVector.setValue("WeekDay", dayOfWeek);
-		
-		// Solve the feature vector by the model
-		// Suggestion contains several fields
-		// Typically the last feature of the feature schema
-		Suggestion suggestion = model.solve(featureVector);
-		System.out.println(suggestion.getFieldValue("Price"));
+		LocalDate endDate = LocalDate.of(2017, 12, 31);
+		while (!date.isAfter(endDate))
+		{
+			FeatureVector featureVector = model.createFeatureVector();
+			featureVector.setValue("Year", date.getYear());
+			featureVector.setValue("Month", date.getMonthValue());
+			featureVector.setValue("Day", date.getDayOfMonth());
+			int dayOfWeek = date.getDayOfWeek().getValue() % DayOfWeek.SUNDAY.getValue();
+			featureVector.setValue("WeekDay", dayOfWeek);
+
+			Suggestion suggestion = model.solve(featureVector);
+			System.out.println(suggestion.getFieldValue("Price"));
+			date = date.plusDays(1);
+		}
 	}
 }
